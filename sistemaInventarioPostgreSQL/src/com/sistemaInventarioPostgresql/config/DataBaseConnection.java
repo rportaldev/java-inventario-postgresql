2e90ecb4fe6abe23cf4d34ab4b252681e9fb06db
Dev Zero <litkaponii@gmail.com>
2026-04-07 22:25:39 -0500

    Agrego la clase DataBaseConnection

--- sistemaInventarioPostgreSQL/src/com/sistemaInventarioPostgresql/config/DataBaseConnection.java
+++ sistemaInventarioPostgreSQL/src/com/sistemaInventarioPostgresql/config/DataBaseConnection.java
@@ -6,6 +6,19 @@ import java.sql.SQLException;
 
 public class DataBaseConnection {
 
+	static String url = "jdbc:postgresql://localhost:5432/db_inventario";
+    static String usuario = "usuario_dev";
+    static String password = "devzero";
+
+    public static Connection getConexion() {
+        try {
+            Connection conn = DriverManager.getConnection(url, usuario, password);
+            return conn;
+        } catch (SQLException e) {
+            System.err.println("Error de conexión: " + e.getMessage());
+            return null;
+        }
+    }
 
 	
 }
